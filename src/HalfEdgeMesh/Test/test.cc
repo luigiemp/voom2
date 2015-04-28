7d2973373661b29216e23f5a8c2f295851997c4a
Sanjay Dharmavaram <sanjaydm@login1.(none)>
2015-04-28 16:38:44 -0700

    Halfedge test passed.

--- src/HalfEdgeMesh/Test/test.cc
+++ src/HalfEdgeMesh/Test/test.cc
@@ -4,15 +4,29 @@ using namespace voom;
 
 int main(){
 
-  HalfEdgeMesh::ConnectivityContainer connect(2);
-  //
-  //
-  // 0---3
-  // | \ |
-  // 1---2
-  connect[0] << 0, 1, 2;
-  connect[1] << 2, 3, 0;
-  int nverts=4;
+  // HalfEdgeMesh::ConnectivityContainer connect(2);
+  // //
+  // //
+  // // 0---3
+  // // | \ |
+  // // 1---2
+  // connect[0] << 0, 1, 2;
+  // connect[1] << 2, 3, 0;
+  // int nverts=4;
+
+  HalfEdgeMesh::ConnectivityContainer connect(4);
+  //         0
+  //       /   \
+  //      1 ___ 2
+  //     / \  / \
+  //    3__ 4 __ 5
+  // 
+  connect[0] << 0, 1, 2; //face 0
+  connect[1] << 1, 4, 2; //face 1
+  connect[2] << 1, 3, 4; //face 2
+  connect[3] << 4, 5, 2; //face 3
+
+  int nverts=6;
 
   HalfEdgeMesh mesh(connect,nverts);
 
