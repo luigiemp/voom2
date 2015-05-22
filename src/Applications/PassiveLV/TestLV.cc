84c58e1b130771ed61164991c8711f1dc6ea51d6
Luigi Perotti <luigiemp@login2.(none)>
2015-05-22 12:55:32 -0700

    Multistep

--- src/Applications/PassiveLV/TestLV.cc
+++ src/Applications/PassiveLV/TestLV.cc
@@ -4,9 +4,10 @@
 #include "EigenEllipticResult.h"
 #include "MechanicsModel.h"
 #include "EigenNRsolver.h"
-#include "LBFGSB.h"
 
 using namespace voom;
+
+void writeNeumannBC(vector<int > BCid, vector<Real > Forces, string OutputFile, int step);
 
 int main(int argc, char** argv)
 {
@@ -24,13 +25,14 @@ int main(int argc, char** argv)
   cout << "Number Of Element : " << LVmesh.getNumberOfElements() << endl;
   cout << "Mesh Dimension    : " << LVmesh.getDimension() << endl << endl;
   
-  // Initialize Material
+  // Initialize Materials and Fibers
   uint NumEl =  LVmesh.getNumberOfElements();
   vector<MechanicsMaterial * > materials;
   materials.reserve(NumEl);
 
   string FiberFile = "CoarseLV.fiber";
   ifstream FiberInp(FiberFile.c_str());
+  int NumMat = 1;
   
   PassMyoA* Mat = new PassMyoA(0, 35.19, 7.06, 100.0, 0.025, 2.87, 2.82);
 
@@ -43,6 +45,7 @@ int main(int argc, char** argv)
     FiberInp >> N(1);
     FiberInp >> N(2);
     Fibers.push_back(N);
+
     // materials.push_back(new CompNeoHookean(k, 10.0, 10.0) );
   }
   LVmesh.setFibers(Fibers);
@@ -52,19 +55,16 @@ int main(int argc, char** argv)
   // Initialize Model
   int NodeDoF = 3;
   int PressureFlag = 1;
-  Real Pressure = 0.05;
-  int NodalForcesFlag = 1;
-  vector<int > ForcesID;
-  vector<Real > Forces;
-  MechanicsModel myModel(&LVmesh, materials, NodeDoF, PressureFlag, Pressure, &LVsurf,
-			 NodalForcesFlag, &ForcesID, &Forces);
+  int NodalForcesFlag = 0;
+  MechanicsModel myModel(&LVmesh, materials, NodeDoF, PressureFlag, &LVsurf,
+			 NodalForcesFlag);
  
   // Initialize Result
   uint PbDoF = (LVmesh.getNumberOfNodes())*myModel.getDoFperNode();
-  EigenEllipticResult myResults(PbDoF, 2);
+  EigenEllipticResult myResults(PbDoF, NumMat*2);
  
   // Print initial configuration
-  myModel.writeOutputVTK("LVpassiveFillingFiber_", 0);
+  myModel.writeOutputVTK("LV_F_", 0);
 
 
 
@@ -84,129 +84,178 @@ int main(int argc, char** argv)
 
   // EBC
   int NumBC = 0, node = 0, ind = 0;;
-  vector<int > DoFid;
-  vector<Real > DoFvalues;
+  vector<int > BCid;
+  vector<Real > BCvalues;
   ifstream BCinp(BCfile.c_str());
 
   BCinp >> NumBC;
-  DoFid.reserve(NumBC*3);
-  DoFvalues.reserve(NumBC*3);
+  BCid.reserve(NumBC*3);
+  BCvalues.reserve(NumBC*3);
   for(int i = 0; i < NumBC; i++) {
     BCinp >> node;
     for (int j = 0; j < 3; j++) {
-      DoFid.push_back(node*3 + j);
-      DoFvalues.push_back(LVmesh.getX(node, j));
-      // cout << DoFid[ind] << " " <<  DoFvalues[ind] << endl;
+      BCid.push_back(node*3 + j);
+      BCvalues.push_back(LVmesh.getX(node, j));
+      // cout << BCid[ind] << " " <<  BCvalues[ind] << endl;
       ind++;
     }
   }
   
-  // Solver
-  Real NRtol = 1.0e-10;
-  uint NRmaxIter = 100;
-  EigenNRsolver mySolver(&myModel, DoFid, DoFvalues, CHOL, NRtol, NRmaxIter);
+ // Loop through pressure
+  int NumPsteps = 30;
+  Real DeltaP = 0.05;
+  /*  
+  {
+    // Solve for displacement first
+    // Solver
+    Real NRtol = 1.0e-10;
+    uint NRmaxIter = 100;
+    EigenNRsolver mySolver(&myModel, BCid, BCvalues, CHOL, NRtol, NRmaxIter);  
 
- int NumPsteps = 30;
- Real DeltaP = 0.05;
-
- for (int i = 1; i <= NumPsteps; i++) {
-      myModel.updatePressure(Real(i)*DeltaP);
+    for (int s = 1; s <= NumPsteps; s++) {
+      myModel.updatePressure(Real(s)*DeltaP);
       mySolver.solve(DISP); 
 
-      myModel.writeOutputVTK("LVpassiveFillingFiber_", i);
-      myModel.writeField("LVpassiveFillingFiber_", i);
+      myModel.writeOutputVTK("LV_F_", s);
+      myModel.writeField("LV_Fdisp_", s);
+      
+      // Compute Residual
+      myResults.setRequest(FORCE);
+      myModel.compute(myResults);
+      VectorXd F = *(myResults._residual);
+      vector<Real > Forces(BCid.size(), 0.0);
+      for (int i = 0; i < BCid.size(); i ++) {
+	Forces[i] = -F(BCid[i]);
+      }
+      writeNeumannBC(BCid, Forces, "LV_Fforces_", s);
     }
-  // mySolver.solve(DISP); 
-  // myModel.writeField(DispFile);
-
-  // Print final configuration
-  // myModel.writeOutputVTK("LVpassiveFillingFiber_", 1);
-
-
-
-  // int m = 5; 
-  // double factr = 1.0e+1;
-  // double pgtol = 1.0e-5;
-  // int iprint = 50;
-  // int maxIterations = 10000;
-  
-  // vector<int > nbd(PbDoF, 0);
-  // vector<double > lowerBound(PbDoF, 0.0);
-  // vector<double > upperBound(PbDoF, 0.0);
-  // for(int i = 0; i < DoFid.size(); i++) {
-  //   nbd[DoFid[i]] = 2;
-  //   lowerBound[DoFid[i]] = DoFvalues[i];
-  //   upperBound[DoFid[i]] = DoFvalues[i];
-  // }
-  // // cout << "PbDoF = " << PbDoF << endl;
-  // // for (int j = 0; j < PbDoF; j++) {
-  // //   cout << nbd[j] << " " << lowerBound[j] << " " << upperBound[j] << endl;
-  // // }
-  
-  // LBFGSB mySolver(& myModel, & myResults, m, factr, pgtol, iprint, maxIterations);
-  // mySolver.setBounds(nbd, lowerBound, upperBound);
-  // mySolver.solve();
-  
-
-
-  /*
+  }
+  // End of displacement solution
+  cout << endl << "End of displacement solution" << endl << endl << endl;
+*/
+ 
 
   // Next recompute material properties using EMFO
   {
-    // Read in displacements
-    ifstream BCinp(DispFile.c_str());
-    int DoF;
-
-    BCinp >> DoF;
-    vector<int > DoFid(DoF, 0);
-    vector<Real > DoFvalues(DoF, 0);
-    for(int i = 0; i < DoF; i++) {
-      DoFid[i] = i;
-      BCinp >> DoFvalues[i];
-      // cout <<  DoFid[i] << " " <<  DoFvalues[i] << endl;
-      myModel.setField(i, DoFvalues[i]);
-    }
-    // myModel.initializeField(DoFvalues[0]);
-    // myModel.printField();
-
-    // Compute Residual
-    myRequest = 2;
-    myResults.setRequest(myRequest);
-    myModel.compute(myResults);
-    VectorXd F = *(myResults._residual);
-    for (int i = 0; i < DoFid.size(); i ++) {
-      ForcesID.push_back( DoFid[i] );
-      Forces.push_back( -F(DoFid[i]) );
-      // cout << F(DoFid[i]) << endl;
-    }
-   
     // Change Material properties
     // Find unique material parameters
     set<MechanicsMaterial *> UNIQUEmaterials;
     for (uint i = 0; i < materials.size(); i++) 
       UNIQUEmaterials.insert(materials[i]);
-    
+
+    vector<Real > ErrorInMatProp;
+    cout << endl << "Original material properties" << endl;
+    for (int k = 0; k < NumMat; k++) {
+      vector<Real > MatProp = materials[k]->getMaterialParameters();
+      int NumPropPerMat = MatProp.size();
+      for (int m = 0; m < NumPropPerMat; m++) {
+	cout << MatProp[m] << " ";
+	ErrorInMatProp.push_back(MatProp[m]);
+      }
+      cout << endl;
+    }
+
+    cout << endl << endl << "Initial guess for material properties" << endl;
     srand (time(NULL));
     for (set<MechanicsMaterial *>::iterator MatIt = UNIQUEmaterials.begin(); MatIt != UNIQUEmaterials.end(); MatIt++) {
       int MatID = (*MatIt)->getMatID();
       vector<Real > MatProp = (*MatIt)->getMaterialParameters();
       int NumPropPerMat = MatProp.size();
       for (int m = 0; m < NumPropPerMat; m++) {
-	MatProp[m] *= double(rand())/double(RAND_MAX);
-	cout << MatProp[m] << " ";
+    	MatProp[m] *= double(rand())/double(RAND_MAX);
+  	cout << MatProp[m] << " ";
       }
       cout << endl;
       (*MatIt)->setMaterialParameters(MatProp);
     }
+    cout << endl << "Material properties identification." << endl;
 
-    myModel.writeOutputVTK("LVpassiveFillingFiber_", 2);
-    
-    // Solve for correct material properties
+    // Need to apply all external forces: pressure + any Neumann BC
+    myModel.setNodalForcesFlag(1);
+
+    // Initialize solver parameters
     Real NRtol = 1.0e-12;
     uint NRmaxIter = 10;
-    EigenNRsolver mySolver(&myModel, DoFid, DoFvalues, CHOL, NRtol, NRmaxIter);
-    mySolver.solve(MAT); 
-    
+    Real error = 1.0;
+    uint iter = 0;
+    uint DeltaSteps = 3;
+
+    // NR loop
+    while (iter < NRmaxIter && error > NRtol)
+    {
+      myResults.setRequest(8);
+      myModel.setResetFlag(1);
+
+      for ( int s = 1; s <= NumPsteps; s+=DeltaSteps ) {
+	
+	cout << "Step considered = " << s << endl;
+
+	int DoF;
+
+	// Read in displacements
+	stringstream DispFileNameStream;
+	DispFileNameStream << "LV_Fdisp_" << s << ".dat";
+	ifstream DBCinp;
+	DBCinp.open( (DispFileNameStream.str()).c_str() );
+
+	DBCinp >> DoF;
+	vector<int > DoFid(DoF, 0);
+	vector<Real > DoFvalues(DoF, 0);
+	for(int i = 0; i < DoF; i++) {
+	  DoFid[i] = i;
+	  DBCinp >> DoFvalues[i];
+	  myModel.setField(i, DoFvalues[i]);
+	}
+   
+	// Read in forces
+	stringstream ForceFileNameStream;
+	ForceFileNameStream << "LV_Fforces_" << s << ".dat";
+	ifstream NBCinp;
+	NBCinp.open( (ForceFileNameStream.str()).c_str() );
+
+	NBCinp >> DoF;
+	vector<int > ForceDoF(DoF, 0);
+	vector<Real > ForceValues(DoF, 0);
+	for(int i = 0; i < DoF; i++) {
+	  NBCinp >> ForceDoF[i];
+	  NBCinp >> ForceValues[i];
+	}
+	myModel.updateNodalForces( &ForceDoF, &(ForceValues) );
+
+	// Update pressure
+	myModel.updatePressure(Real(s)*DeltaP);
+
+	// Compute Hg and Gradg
+	myModel.compute(myResults);
+	myModel.setResetFlag(0);
+      }
+
+      VectorXd DeltaAlpha;
+      SimplicialCholesky<SparseMatrix<Real > > chol(*(myResults._Hg));  // performs a Cholesky factorization of _stiffness
+      DeltaAlpha = chol.solve(*(myResults._Gradg));                             // use the factorization to solve for the given right hand side
+
+      // Update material properties in material
+      for (set<MechanicsMaterial *>::iterator MatIt = UNIQUEmaterials.begin(); MatIt != UNIQUEmaterials.end(); MatIt++) {
+	int MatID = (*MatIt)->getMatID();
+	vector<Real > MatProp = (*MatIt)->getMaterialParameters();
+	int NumPropPerMat = MatProp.size();
+	for (int m = 0; m < NumPropPerMat; m++) {
+	  MatProp[m] -= DeltaAlpha(MatID*NumPropPerMat + m);
+	}
+	(*MatIt)->setMaterialParameters(MatProp);
+      }
+
+      // Update iter and error
+      iter++;
+      error = DeltaAlpha.norm();
+      myResults.setRequest(ENERGY);
+      myModel.compute(myResults);
+      cout << "Energy = " << myResults.getEnergy() << "   - NR iter = " << iter << "   -  NR error = " << error << endl;
+      
+    } // while loop  
+   
+    cout << endl << "Identified material properties" << endl;
+
     // Print found material properties
     for (set<MechanicsMaterial *>::iterator MatIt = UNIQUEmaterials.begin(); MatIt != UNIQUEmaterials.end(); MatIt++) {
       int MatID = (*MatIt)->getMatID();
@@ -217,18 +266,47 @@ int main(int argc, char** argv)
       }
       cout << endl;
     }
-
-    myModel.writeOutputVTK("LVpassiveFillingFiber_", 3);
     
+    int ind = 0;
+    for (int k = 0; k < NumMat; k++) {
+      vector<Real > MatProp = materials[k]->getMaterialParameters();
+      int NumPropPerMat = MatProp.size();
+      for (int m = 0; m < NumPropPerMat; m++) {
+	ErrorInMatProp[ind] -= MatProp[m];
+	ind++;
+      }
+    }
+
+    Real MatPropError = 0.0;
+    for (int i = 0; i < ErrorInMatProp.size(); i++) {
+      MatPropError += ErrorInMatProp[i]*ErrorInMatProp[i];
+    }
+
+    cout << "L2 norm of Mat Prop Error = " << pow(MatPropError, 0.5) << endl;
   } // End of material properties solution
-  
-  */
+
+
 
   // Timing
   time (&end);
   cout << endl << "BVP solved in " << difftime(end,start) << " s" << endl;
-  
-
-  
+    
   return 0;
 }
+
+
+
+void writeNeumannBC(vector<int > BCid, vector<Real > Forces, string OutputFile, int step) {
+  // Create outputFile name
+  stringstream FileNameStream;
+  FileNameStream << OutputFile << step << ".dat";
+  ofstream out;
+  out.open( (FileNameStream.str()).c_str() );
+  
+  out << BCid.size() << endl;
+  for (uint i = 0; i < BCid.size(); i++) {
+    out << BCid[i] << " " << setprecision(15) << Forces[i] << endl;
+  }
+  out.close();
+}
+
