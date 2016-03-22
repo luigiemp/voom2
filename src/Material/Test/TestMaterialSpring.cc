1f82c561e2819a7173943e5a809be4bebf621f61
LouisFoucard <foucard@colorado.edu>
2016-03-22 14:57:20 -0700

    check consistency

--- src/Material/Test/TestMaterialSpring.cc
+++ src/Material/Test/TestMaterialSpring.cc
@@ -14,45 +14,26 @@ int main()
     cout << endl << "Testing Spring material. " << endl;
     Real k = 1.0;
     Vector3d d0;
-
+    Vector3d d;
     d0 << 1.0, 0.0, 0.0;
-        
+    d << 1.1,0.0,0.0;
+     
     Spring MatSpring(0, k, d0);
      
     FilamentMaterial::Filresults Rf;
     
     Rf.request = 7;
 
-    MatSpring.compute(Rf,d0);
+    MatSpring.compute(Rf,d);
 
     cout << "Energy     = " << Rf.W << endl;    
     cout << "Force  = " << Rf.f << endl;                                                                                                                                                                                             
     cout << "Stiffness = " << Rf.k << endl;   
-    /*
-    Matrix3d F;
-    F << 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0;
-    srand(time(NULL));
-    for (unsigned int i = 0; i<3; i++) 
-      for (unsigned int J = 0; J<3; J++) 
-	F(i,J) += 0.1*(double(rand())/RAND_MAX);
-    cout << "determinant(F) = " << F.determinant() << endl;
     
-
-    MatMech.compute(Rm, F);
-    
-    cout << "Energy     = " << Rm.W << endl;
-    cout << "P(2,2)     = " << Rm.P(2,2) << endl;
-    cout << "K[0,0,0,0] = " << Rm.K.get(0,0,0,0) << endl;
-    // for (unsigned int i = 0; i<3; i++) {
-    //   for (unsigned int J = 0; J<3; J++) {
-    // 	cout << i << " " << J << " " << (Rm.Dmat).get( 0, i, J ) << " " << (Rm.Dmat).get( 1, i, J ) << endl;
-    //   }
-    // }
-    
-    cout << endl << "Material ID = " << MatMech.getMatID() << endl << endl;
+    cout << endl << "Material ID = " << MatSpring.getMatID() << endl << endl;
  
-    MatMech.checkConsistency(Rm,F);
-    */
+    MatSpring.checkConsistency(Rf,d);
+
   }
 
 
