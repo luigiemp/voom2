9a40bea400c37881d2c5329809059704d5e779db
LouisFoucard <foucard@colorado.edu>
2016-04-12 17:59:39 -0700

    filament model

--- src/Model/Test/TestGelModel.cc
+++ src/Model/Test/TestGelModel.cc
@@ -24,15 +24,16 @@ int main(int argc, char** argv) {
 
     uint NumFil = TestGelMesh.getNumberOfFilaments();
     
+    uint PbDoF = NumFil*NodeDoF;
+
     std::cout << "Number of elements is " << NumFil << std::endl;
     
     vector<FilamentMaterial * > materials;
     materials.reserve(NumFil);
     
-    Real k = 1.0;
+    Real k;
     Vector3d d0;
-    d0 << 1.0, 0.0, 0.0;
-    
+        
     for (int nFil = 0; nFil < NumFil; nFil++) {        
       Spring* Mat = new Spring(nFil,k,d0);
       materials.push_back(Mat);
@@ -40,66 +41,16 @@ int main(int argc, char** argv) {
     
     GelModel myModel(&TestGelMesh, materials, NodeDoF,0,1);
     
+    EigenResult R(PbDoF, 1);
+   
+    R.setRequest(7);
      
-    myModel.initializeField(1.0);    
-    const vector<GeomFilament* > filElement = TestGelMesh.getFilaments();    
-
-    for (int nFil = 0 ; nFil < NumFil ; nFil++){
-
-      const vector<int > & NodesID = filElement[nFil]->getNodesID();
-      const uint nodeNum = NodesID.size();
-      vector<Vector3d > dlist(nodeNum-1,Vector3d::Zero());
-      
-      myModel.computeDeformation(dlist,filElement[nFil]);
-      
-      FilamentMaterial::Filresults Rf;
-      Rf.request = 7;
-      for(int n = 0; n<nodeNum-1; n++)
-	{
-	  cout << dlist[n] << endl;
-	  vector<int> bond;
-	  bond.push_back(NodesID[n]);
-	  bond.push_back(NodesID[n+1]);
-	  Vector3d d0;
-	  cout << "OK" << endl;
-	  d0[0] = TestGelMesh.getX(bond[0],0)-TestGelMesh.getX(bond[1],0);
-	  d0[1] = TestGelMesh.getX(bond[0],1)-TestGelMesh.getX(bond[1],1);
-	  d0[2] = TestGelMesh.getX(bond[0],2)-TestGelMesh.getX(bond[1],2);
-	  cout << "d0" << endl;
-	  cout << d0 << endl;
-	  cout << dlist[n] << endl;
-	  
-	  materials[nFil]->compute(Rf,d0);
-	  cout << "Energy     = " << Rf.W << endl;
-	  cout << "Force  = " << Rf.f << endl;   
-	  cout << "Stiffness = " << Rf.k << endl;
-	}
-    }    
+    myModel.initializeField(1.1);    
     
-    // Run consistency test                                           
-    uint PbDoF = (TestGelMesh.getNumberOfNodes())*myModel.getDoFperNode();
+    myModel.compute(R);
+
     
-    cout << "pbdof : " << PbDoF << endl;
-
-    /*
-    MechanicsModel myModel(&myFEmesh, materials, NodeDoF, PressureFlag, Pressure, &surfMesh);
-    
-    // Run consistency test
-    uint PbDoF = (myFEmesh.getNumberOfNodes())*myModel.getDoFperNode();
-    int TotNumMatProp = NumMat*2;
-    EigenEllipticResult myResults(PbDoF, TotNumMatProp);
-
-    Real perturbationFactor = 0.1;
-    uint myRequest = 6; // Check both Forces and Stiffness
-    Real myH = 1e-6;
-    Real myTol = 1e-7;
-
-    myModel.checkConsistency(myResults, perturbationFactor, myRequest, myH, myTol);
-  
-
-    */
-
-    cout << endl << " END OF TEST OF MECHANICS MODEL " << endl;
+    cout << endl << " END OF TEST OF GEL MODEL " << endl;
     cout << " ------------------------------ " << endl << endl;
   }
 
