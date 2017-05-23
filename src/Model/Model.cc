807092281c917f344aee27376a862b5b12debb4f
Luigi Perotti <luigiemp@gmail.com>
2017-05-23 15:55:33 -0700

    New Model that takes a vector of bodies

--- src/Model/Model.cc
+++ src/Model/Model.cc
@@ -3,59 +3,71 @@
 
 namespace voom {
 
+  // Compute //
+  void Model::compute(Result* R) {
+    for (int i = 0; i < _bodies.size(); i++) {
+      _bodies[i]->compute(R);
+    }
+  }
+
+
+
   // Consistency Checks //
-  void Model::checkConsistency(Result* R, Real perturbationFactor, int request,
-				       Real h, Real tol)
+  void Model::checkConsistency(Result* R,  int nodeNum, int nodeDoF, 
+			       Real perturbationFactor, 
+			       int request = 6, Real h = 1e-6, Real tol = 1e-6);
   {
     // Check only for local nodes
-    const uint nodeNum   = _myMesh->getNumberOfNodes();
-    const uint nLocalDoF = nodeNum*_nodeDoF;
+    const int nodeNum   = _myMesh->getNumberOfNodes();
+    const int nLocalDoF = nodeNum * nodeDoF;
 
     // Perturb field randomly to change from reference configuration
     // Save perturbed field to set the configuration back to reference 
     // at the end of the test
     vector<Real > perturb(nLocalDoF, 0.0);
     srand( time(NULL) );
-    for(uint a = 0; a < nodeNum; a++) {
-      for(uint i = 0; i < _nodeDoF; i++) {
+    for(int a = 0; a < nodeNum; a++) {
+      for(int i = 0; i < nodeDoF; i++) {
 	Real randomNum =  perturbationFactor*(Real(rand())/RAND_MAX - 0.5);
-	perturb[a*_nodeDoF + i] = randomNum;
-	this->linearizedUpdate(a, i, randomNum);
+	perturb[a*nodeDoF + i] = randomNum;
+	R->linearizedUpdate(a*nodeDoF + i, randomNum);
       }
     }      
-    
-
 
     // Force Check
     if ( request & FORCE ) {
       Real error = 0.0, norm = 0.0;
 
-      R->setRequest(3); // First compute forces numerically
+      R->setRequest(FORCE); // First compute forces numerically
+      R->resetResults(FORCE);
       this->compute(R);
 
-      R->setRequest(1); // Reset result request so that only energy is computed 
-      // this->compute(R);
+      R->setRequest(ENERGY); // Reset result request so that only energy is computed 
+      R->resetResults(ENERGY);
+      this->compute(R);
 
       cout << "Model energy at test start = " <<  R->getEnergy() << endl;
 
       for(int a = 0; a < nodeNum; a++) {
-	for(int i = 0; i < _nodeDoF; i++) {
+	for(int i = 0; i < nodeDoF; i++) {
 	  // Perturb +h
-	  this->linearizedUpdate(a, i, h);
+	  R->linearizedUpdate(a*nodeDoF + i, h);
+	  R->resetResults(ENERGY);
 	  this->compute(R);
 	  Real Wplus = R->getEnergy();
 	  
 	  // Perturb -2h
-	  this->linearizedUpdate(a, i, -2*h);
+	  R->linearizedUpdate(a*nodeDoF + i, -2.0*h);
+	  R->resetResults(ENERGY);
 	  this->compute(R);
 	  Real Wminus = R->getEnergy();
 	  
 	  // Bring back to original position
-	  this->linearizedUpdate(a, i, h);
+	  R->linearizedUpdate(a*nodeDoF + i, h);
 	  
-	  error += pow( (Wplus-Wminus)/(2.*h) - 
-			R->getResidual(a*_nodeDoF + i), 2);
-	  norm += pow(R->getResidual(a*_nodeDoF + i), 2);
+	  error += pow( (Wplus-Wminus)/(2.0*h) - 
+			R->getResidual(a*nodeDoF + i), 2);
+	  norm += pow(R->getResidual(a*nodeDoF + i), 2);
 	} // Loop over dimension
       } // Loop over nodes
       error = sqrt(error);
@@ -71,42 +83,44 @@ namespace voom {
       }
     } // Check Forces loop
 
-
-
     // Stiffness check
     if ( request & STIFFNESS ) {
       Real error = 0.0, norm = 0.0;
 
-      R->setRequest(4); // First compute stiffness numerically
+      R->setRequest(STIFFNESS); // First compute stiffness numerically
+      R->resetResults(STIFFNESS);
       this->compute(R);
+      R->FinalizeGlobalStiffnessAssembly();
 
-      R->setRequest(2); // Reset result request so that only forces are computed 
+      R->setRequest(FORCE); // Reset result request so that only forces are computed 
       for(int a = 0; a < nodeNum; a++) {
-	for(int i = 0; i < _nodeDoF; i++) {
+	for(int i = 0; i < nodeDoF; i++) {
 	  for(int b = 0; b < nodeNum; b++) {
-	    for(int j = 0; j < _nodeDoF; j++) {
+	    for(int j = 0; j < nodeDoF; j++) {
 	      // Perturb +h
-	      this->linearizedUpdate(b, j, h);
+	      R->linearizedUpdate(b*nodeDoF + j, h);
+	      R->resetResults(FORCE);
 	      this->compute(R);
-	      Real Fplus = R->getResidual(a*_nodeDoF + i);
+	      Real Fplus = R->getResidual(a*nodeDoF + i);
 
 	      // Perturb -2h
-	      this->linearizedUpdate(b, j, -2*h);
+	      R->linearizedUpdate(b*nodeDoF + j, -2*h);
+	      R->resetResults(FORCE);
 	      this->compute(R);
-	      Real Fminus = R->getResidual(a*_nodeDoF + i);
+	      Real Fminus = R->getResidual(a*nodeDoF + i);
 
 	      // Bring back to original position
-	      this->linearizedUpdate(b, j, h);
+	      R->linearizedUpdate(b*nodeDoF + j, h);
 
 	      // Computing Error and Norm;
-	      error += pow((Fplus - Fminus)/(2.*h) - R->getStiffness(a*_nodeDoF+i, b*_nodeDoF+j), 2.0);
-	      norm += pow( R->getStiffness(a*_nodeDoF+i, b*_nodeDoF+j), 2.0); 
-	      // cout << R->getStiffness(a*_nodeDoF+i, b*_nodeDoF+j) << "\t" << (Fplus - Fminus)/(2.*h) << endl;
+	      error += pow((Fplus - Fminus)/(2.*h) - R->getStiffness(a*nodeDoF+i, b*nodeDoF+j), 2.0);
+	      norm += pow( R->getStiffness(a*nodeDoF+i, b*nodeDoF+j), 2.0); 
+	      // cout << R->getStiffness(a*nodeDoF+i, b*nodeDoF+j) << "\t" << (Fplus - Fminus)/(2.*h) << endl;
 	    } // j loop
 	  } // b loop
 	} // i loop
       } // a loop
-      
+
       error = sqrt(error);
       norm  = sqrt(norm);
       if ( abs(error) < norm * tol) {
@@ -120,13 +134,14 @@ namespace voom {
     } // Check Stiffness    
 
     // Reset field to initial values
-    for(uint a = 0; a < nodeNum; a++) {
-      for(uint i = 0; i < _nodeDoF; i++) {
-	this->linearizedUpdate(a, i, -perturb[a*_nodeDoF + i]);
+    for(int a = 0; a < nodeNum; a++) {
+      for(int i = 0; i < nodeDoF; i++) {
+	R->linearizedUpdate(a*nodeDoF + i, -perturb[a*nodeDoF + i]);
       }
     } 
     
   } // Check consistency
+
 
 
 } // Namespace voom
--- src/Model/Model.h
+++ src/Model/Model.h
@@ -8,7 +8,7 @@
 #define __Model_h__
 
 #include "voom.h"
-#include "Mesh.h"
+#include "Body.h"
 #include "Result.h"
 
 namespace voom {
@@ -21,80 +21,22 @@ namespace voom {
     // Model(Mesh* myMesh, const string inputFile, const uint NodeDoF);
 
     //! Constructor to be used
-    Model(Mesh* myMesh, const uint NodeDoF, Result* myResult)
-      : _myMesh(myMesh), _nodeDoF(NodeDoF), _myResult(myResult) {
-      // WSK: evetually we want to copy the mesh object that is
-      // pointed to.  To do this we need to implement a virtual copy
-      // method to be used like this:
-      //
-    }
+    Model(vector<Body *> Bodies, Result* ModelResult): _bodies(Bodies) _modelResult(ModelResult) {} ;
 
     //! Destructor
-    virtual ~Model() {};
+    ~Model() {};
 
     //! Model compute function
-    virtual void compute(Result* R) = 0;
+    void compute(Result* R);
 
     //! Check consistency - common to all models
-    void checkConsistency(Result* R, Real perturbationFactor, int request = 6,
-			  Real h = 1e-6, Real tol = 1e-6);
-
-    //! Return number of unique material pointers
-    virtual uint getNumMat() = 0;
-
-    //! Get DoF per Node
-    uint getDoFperNode() {
-      return _nodeDoF;
-    }
-    
-    //! GetMesh
-    Mesh * getMesh() {
-      return _myMesh;
-    }
-
-    //! Initialize field
-    // From constant value
-    virtual void initializeField(const Real value = 0.0) = 0;
- 
-    //! From array
-    virtual void initializeField(const Real* value) = 0;
-
-    //! Linearized update (local and ghost solution)
-    // From solution array (Used by Solver)
-    // virtual void linearizedUpdate(const Real* locaValues, 
-    // 				  const Real* ghostValues) = 0;
-    virtual void linearizedUpdate(const Real* locaValues, Real fact) = 0;
-
-    // One value at the time (Node ID, DoF index, value)
-    virtual void linearizedUpdate(const int id, const int dof, const Real value) = 0;
-
-    // One value at the time (DoF index, value)
-    virtual void linearizedUpdate(const int dof, const Real value) = 0;
-
-    // Set solution field
-    virtual void setField(uint dof, Real value) = 0;
-    virtual void setField(const Real* value) = 0;
-
-    // Get solution field
-    virtual void getField(vector<double> & x) = 0;
-
-    //! Set Previous Field
-    virtual void setPrevField() = 0;
-
-    // Print field
-    virtual void printField() = 0;
-
-    //! Output generation
-    virtual void writeOutputVTK(const string OutputFile, int step) = 0;
-
-
+    void checkConsistency(Result* R,  int nodeNum, int nodeDoF, 
+			  Real perturbationFactor, 
+			  int request = 6, Real h = 1e-6, Real tol = 1e-6);
 
   protected:
-    Mesh*        _myMesh;
-    Result*      _myResult;
-
-    //! DoF per node
-    uint         _nodeDoF;
+    vector<Body *> _bodies;
+    Result*        _modelResult;
   
   }; // Model
 
